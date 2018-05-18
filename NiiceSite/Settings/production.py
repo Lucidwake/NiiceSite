1f605e2c3da95dae46ca33bd034bb2a9a443052e
Steve Crooke <lucidwake@gmail.com>
2018-05-18 20:16:45 +0800

    PythonAnywhere changes

--- NiiceSite/Settings/production.py
+++ NiiceSite/Settings/production.py
@@ -4,10 +4,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
